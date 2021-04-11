9b22992c7661f0a650985dc417156eb75f4743eb
Muzaffer <aydogdum38@gmail.com>
2021-04-12 00:37:10 +0300

    finished redux-intro

--- redux-intro/src/redux/reducer/configureStore.js
+++ redux-intro/src/redux/reducer/configureStore.js
@@ -1,3 +1,7 @@
 import {createStore} from "redux";
-import counterReducer from "./index";
+import reducers from "./index";
 
+
+export default function configureStore(){
+    return createStore(reducers)
+}
