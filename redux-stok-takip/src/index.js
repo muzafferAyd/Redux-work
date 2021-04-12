4f14a3a98467d03ca78f9fcbaab19be44f495097
Muzaffer <aydogdum38@gmail.com>
2021-04-12 17:37:30 +0300

    established redux

--- redux-stok-takip/src/index.js
+++ redux-stok-takip/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Provider} from "react-redux";
+import configureStore  from "./redux/reducers/configureStore"
+
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
