9e2540f5c140d79f1941e012aaf2f1762d24a749
Muzaffer <aydogdum38@gmail.com>
2021-04-13 17:13:24 +0300

    add changeCategory

--- redux-stok-takip/src/redux/reducers/categoryListReducer.js
+++ redux-stok-takip/src/redux/reducers/categoryListReducer.js
@@ -2,11 +2,11 @@ import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
 export default function changeCategoryReducer(
-  state = initialState.currentCategory,
+  state = initialState.categories,
   action
 ) {
   switch (action.type) {
-    case actionTypes.CHANGE_CATEGORY:
+    case actionTypes.GET_CATEGORIES_SUCCESS:
       return action.payload;
     default:
       return state;
--- redux-stok-takip/src/redux/reducers/initialState.js
+++ redux-stok-takip/src/redux/reducers/initialState.js
@@ -1,3 +1,4 @@
 export default {
-    currentCategory : {categoryName: "Baverages"}
+    currentCategory : {categoryName: "Baverages"},
+    categories:[]
 }
