9cd1e434221dc40bd657ca684adac25c02bc7838
Muzaffer <aydogdum38@gmail.com>
2021-04-15 23:42:11 +0300

    add thunk

--- redux-stok-takip/src/components/categories/CategoryList.js
+++ redux-stok-takip/src/components/categories/CategoryList.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
- class CategoryList extends Component {
-    render() {
-        return (
-            <div >
-                <h3>Categories</h3>
-                <h5>Seçili Kategory : {this.props.currentCategory.categoryName}</h5>
-            </div>
-        )
-    }
+class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Categories {this.props.categories.length} </h3>
+        <ListGroup>
+          {this.props.categories.map(category => (
+            <ListGroupItem key={category.id}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <h5>Seçili Kategory : {this.props.currentCategory.categoryName}</h5>
+      </div>
+    );
+  }
 }
-function mapStateToProps(state){
-    return{
-        currentCategory:state.changeCategoryReducer
-    }
+function mapStateToProps(state) {
+  return {
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+    },
+  };
+}
 
-export default connect(mapStateToProps)(CategoryList)
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
