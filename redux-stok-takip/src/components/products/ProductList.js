418d42fbdd53b8e66496396d49a9a4c1bd21f725
Muzaffer <aydogdum38@gmail.com>
2021-04-16 17:26:23 +0300

    added productListReducer

--- redux-stok-takip/src/components/products/ProductList.js
+++ redux-stok-takip/src/components/products/ProductList.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Badge } from "reactstrap";
+import * as productActions from "../../redux/actions/productActions";
+import { bindActionCreators } from "redux";
+import { Table } from "reactstrap";
 
 class ProductList extends Component {
+  componentDidMount() {
+    this.props.actions.getProducts();
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +18,28 @@ class ProductList extends Component {
           <Badge color="success">
             {this.props.currentCategory.categoryName}
           </Badge>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Product Name</th>
+                <th>Unit Price</th>
+                <th>Quantity PerUnit</th>
+                <th>Units in Stock</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.products.map((product) => (
+                <tr key={product.id}>
+                  <th scope="row">{product.id}</th>
+                  <td>{product.productName}</td>
+                  <td>{product.unitPrice}</td>
+                  <td>{product.quantityPerUnit}</td>
+                  <td>{product.unitsInStock}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </h3>
       </div>
     );
@@ -20,6 +49,16 @@ class ProductList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    products: state.productListReducer,
   };
 }
-export default connect(mapStateToProps)(ProductList);
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getProducts: bindActionCreators(productActions.getProducts, dispatch),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
