0f292a5a940dd48af4f146017325f89f0836039a
Muzaffer <aydogdum38@gmail.com>
2021-04-17 10:35:58 +0300

    added cart in redux

--- redux-stok-takip/src/components/products/ProductList.js
+++ redux-stok-takip/src/components/products/ProductList.js
@@ -18,29 +18,29 @@ class ProductList extends Component {
           <Badge color="success">
             {this.props.currentCategory.categoryName}
           </Badge>
-          <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Product Name</th>
-                <th>Unit Price</th>
-                <th>Quantity PerUnit</th>
-                <th>Units in Stock</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.products.map((product) => (
-                <tr key={product.id}>
-                  <th scope="row">{product.id}</th>
-                  <td>{product.productName}</td>
-                  <td>{product.unitPrice}</td>
-                  <td>{product.quantityPerUnit}</td>
-                  <td>{product.unitsInStock}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
         </h3>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Unit Price</th>
+              <th>Quantity PerUnit</th>
+              <th>Units in Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => (
+              <tr key={product.id}>
+                <th scope="row">{product.id}</th>
+                <td>{product.productName}</td>
+                <td>{product.unitPrice}</td>
+                <td>{product.quantityPerUnit}</td>
+                <td>{product.unitsInStock}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
